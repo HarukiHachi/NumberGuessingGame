29613682157eb5f283cfbf33a7d0665ed4e348a5
Haruki <ldc.illusion99@gmail.com>
2021-09-09 22:20:45 +0700

    delete file exe and update game hints

--- newNumber.py
+++ newNumber.py
@@ -14,51 +14,69 @@ def guessingNumber():
         if playerNumber < chooseTheNumberForPlayer:
             if (chooseTheNumberForPlayer % 2 == 0):
                 print("Your number is lower than the given number which can divisible by 2")
-                helpPlayer1 = []
-                for i in range(0, int(chooseTheNumberForPlayer)):
-                    if (i % 2 == 0):
-                        helpPlayer1.append(i)
-                print("Try these numbers:", helpPlayer1, "if doesn't works try plus 2 to highest number on this list")
+                if (newRange >= 50):
+                    helpPlayer1 = []
+                    for i in range(0, int(chooseTheNumberForPlayer)):
+                        if (i % 2 == 0):
+                            helpPlayer1.append(i)
+                    print("Try these numbers:", helpPlayer1, "if doesn't works try plus 2 to highest number on this list")
+                else:
+                    print("You can do it :D")
             elif (chooseTheNumberForPlayer %3 == 0):
                 print("Your number is lower than the given number which can divisible by 3")
-                helpPlayer2 = []
-                for i in range(0, int(chooseTheNumberForPlayer)):
-                    if (i % 3 == 0):
-                        helpPlayer2.append(i)
-                print("Try these numbers:", helpPlayer2, "if doesn't works try plus 3 to highest number on this list")
+                if (newRange >= 50):
+                    helpPlayer2 = []
+                    for i in range(0, int(chooseTheNumberForPlayer)):
+                        if (i % 2 == 0):
+                            helpPlayer2.append(i)
+                    print("Try these numbers:", helpPlayer2, "if doesn't works try plus 2 to highest number on this list")
+                else:
+                    print("You can do it :D")
             else:
                 print("Your number is lower than the given number either divisible by 3 nor 2")
-                helpPlayer3 = []
-                for i in range(playerNumber, chooseTheNumberForPlayer):
-                    if (i % 2 != 0) and (i % 3 != 0):
-                        helpPlayer3.append(i)
-                helpPlayer3.append(chooseTheNumberForPlayer)
-                print("Try these numbers", helpPlayer3)
+                if (newRange >= 50):
+                    helpPlayer3 = []
+                    for i in range(playerNumber, chooseTheNumberForPlayer):
+                        if (i % 2 != 0) and (i % 3 != 0):
+                            helpPlayer3.append(i)
+                    helpPlayer3.append(chooseTheNumberForPlayer)
+                    print("Try these numbers", helpPlayer3)
+                else:
+                    print("You can do it :D")
 
             playerNumber = int(input("Nice guess, but not enough, try again: "))
         else:
             if (chooseTheNumberForPlayer % 2 == 0):
                 print("Your number is higher than the given number which can divisible by 2")
-                helpPlayer4 = []
-                for i in range(0, int(chooseTheNumberForPlayer)):
-                    if (i % 2 == 0):
-                        helpPlayer4.append(i)
-                print("Try these numbers:", helpPlayer4, "if doesn't works try plus 2 to highest number on this list")
+                if (newRange >= 50):
+                    helpPlayer4 = []
+                    for i in range(0, int(chooseTheNumberForPlayer)):
+                        if (i % 2 == 0):
+                            helpPlayer4.append(i)
+                    print("Try these numbers:", helpPlayer4, "if doesn't works try plus 2 to highest number on this list")
+                else:
+                    print("You can do it :D")
             elif (chooseTheNumberForPlayer % 3 == 0):
                 print("Your number is higher than the given number which can divisible by 3")
-                helpPlayer5 = []
-                for i in range(0, int(chooseTheNumberForPlayer)):
-                    if (i % 3 == 0):
-                        helpPlayer5.append(i)
-                print("Try these numbers:", helpPlayer5, "if doesn't works try plus 3 to highest number on this list")
+                if (newRange >= 50):
+                    helpPlayer5 = []
+                    for i in range(0, int(chooseTheNumberForPlayer)):
+                        if (i % 3 == 0):
+                            helpPlayer5.append(i)
+                    print("Try these numbers:", helpPlayer5, "if doesn't works try plus 3 to highest number on this list")
+                else:
+                    print("You can do it :D")
             else:
                 print("Your number is higher than the given number either divisible by 3 nor 2")
-                helpPlayer6 = []
-                for i in range(playerNumber, chooseTheNumberForPlayer):
-                    if (i % 2 != 0) and (i % 3 != 0):
-                        helpPlayer6.append(i)
-                    helpPlayer6.append(chooseTheNumberForPlayer)
-                print("Try these numbers", helpPlayer6)
+                if (newRange >= 50):
+                    helpPlayer6 = []
+                    for i in range(playerNumber, chooseTheNumberForPlayer):
+                        if (i % 2 != 0) and (i % 3 != 0):
+                            helpPlayer6.append(i)
+                        helpPlayer6.append(chooseTheNumberForPlayer)
+                    print("Try these numbers", helpPlayer6)
+                else:
+                    print("You can do it :D")
             playerNumber = int(input("Nice guess, but not enough, try again: "))
     else:
         print("Congratulations, you have done the game!")
